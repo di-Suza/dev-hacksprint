99ea29d6b08285016ee8b6cd8315b0adecd2ee94
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-15 19:27:18 +0530

    feat: url fix

--- api/src/app.js
+++ api/src/app.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "https://devhacksprint.netlify.app",
-    origin: "http://localhost:5173",
+    origin: "https://devhacksprint.netlify.app",
+    // origin: "http://localhost:5173",
     credentials: true, // for cookies
   }),
 );
--- web/src/shared/api/baseQuery.js
+++ web/src/shared/api/baseQuery.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// const url = "https://dev-hacksprint.onrender.com/api";
-const url = "http://localhost:8080/api"
+const url = "https://dev-hacksprint.onrender.com/api";
+// const url = "http://localhost:8080/api"
 export const baseQuery = fetchBaseQuery({
   baseUrl: url,
   credentials: "include",
